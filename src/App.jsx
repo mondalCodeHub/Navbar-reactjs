2f57812e1c0437b4ae9411693311d01ac756a241
Arup Mondal <mondalconnect17@gmail.com>
2022-11-23 13:15:52 +0530

    App component

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { useState } from 'react'
+import Navbar from './Navbar'
+
+
+function App() {
+  return (
+    <>
+      <Navbar />
+    </>
+  )
+}
+
+export default App
+// @mondalcodehub-NOV22
